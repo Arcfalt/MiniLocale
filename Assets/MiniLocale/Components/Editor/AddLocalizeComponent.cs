5da25b91c0fc6deac04903ecbfbd4b8010d1a8c9
Arcfalt <me@arcfalt.com>
2020-07-19 11:10:58 +1000

    Context menu action for auto-adding localizer component

---
+++ Assets/MiniLocale/Components/Editor/AddLocalizeComponent.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEngine.UI;
+
+namespace MiniLocale.Components.Editor
+{
+	public static class AddLocalizeComponent
+	{
+		[MenuItem("CONTEXT/Text/Add Localizer")]
+		private static void AddUguiLocalizer(MenuCommand mc)
+		{
+			var text = mc.context as Text;
+			if (text == null) return;
+			var go = text.gameObject;
+			go.AddComponent<LocalizeUguiText>();
+		}
+		
+		[MenuItem("CONTEXT/TMP_Text/Add Localizer")]
+		private static void AddTextMeshLocalizer(MenuCommand mc)
+		{
+			var text = mc.context as TMP_Text;
+			if (text == null) return;
+			var go = text.gameObject;
+			go.AddComponent<LocalizeTextMeshPro>();
+		}
+	}
+}
